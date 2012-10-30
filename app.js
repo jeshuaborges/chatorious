23b64f942b3945ef8e3464108e2e2c7d1bf4b932
Jeshua Borges <jesh@jesh.me>
2012-10-30 16:33:59 -0700

    introduce Rdio API

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ ss.http.route('/', function(req, res){
 ss.client.formatters.add(require('ss-less'));
 ss.client.templateEngine.use('ember');
 
+
+// The socketstream static host does not return a static file when there is a
+// querystring present.
+ss.client.define('helper', {
+  view: 'helper.html'
+});
+
+ss.http.route('/helper.html', function(req, res){
+  res.serveClient('helper');
+});
+
+
 if (ss.env === 'production') {
   ss.client.packAssets();
 }
